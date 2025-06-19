6242734218e6b3587e1f67ff6ebf376cb546c2f5
Domas Tamašauskas <puerdomus@gmail.com>
2025-06-19 18:28:09 +0300

    Add an executable target for testing the library because the repl doesn't work

--- Package.swift
+++ Package.swift
@@ -5,19 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "ProcessMemory",
-	platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ProcessMemory",
             targets: ["ProcessMemory"]
-		),
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ProcessMemory",
-		),
+        .target(name: "ProcessMemory"),
+        .executableTarget(name: "mem", dependencies: ["ProcessMemory"]),
     ]
 )
---
+++ Sources/mem/mem.swift
@@ -0,0 +1,28 @@
+import ProcessMemory
+
+extension ArraySlice where Element: Comparable {
+    func get(atIndex: Index) -> Element? {
+        return indices.contains(atIndex) ? self[atIndex] : nil
+    }
+}
+
+@main
+struct MemMain {
+    static func main() {
+        let args = CommandLine.arguments.dropFirst()
+        guard let pos = args.firstIndex(where: { $0 == "--pid" }) else {
+            return
+        }
+
+        guard let stringOfPid = args.get(atIndex: pos + 1) else {
+            return
+        }
+
+        guard let pid = Int32(stringOfPid) else {
+            return
+        }
+
+        let addr = getBaseAddress(for: pid)
+        print(addr)
+    }
+}
